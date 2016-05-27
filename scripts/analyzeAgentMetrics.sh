10d5a211461bcc2fbf2fcef5aa35a8fdf687d334
Kenneth McKnight <kmcknight@electric-cloud.com>
2016-05-27 15:28:18 -0700

    Rework functions to allow for specification of each table via a command line option; also create bandwidth table

--- scripts/analyzeAgentMetrics.sh
+++ scripts/analyzeAgentMetrics.sh
@@ -43,14 +43,16 @@ progname=$0
 
 function usage () {
    cat <<EOF
-Usage: $progname [-d dstMetricsSummary] [-ahou] metricsSummary
+Usage: $progname [-ahou] [-f file] metricsSummary
 
 Options:
-   -a                    Specify all analysis options (same as -ou)
-   -d dstMetricsSummary  Side by side diff of two agent metrics summary files
-   -h                    help
-   -o                    Specify overall time usage performance analysis
-   -u                    Specify usage record performance analysis
+   -a       All analysis options (same as -bou)
+   -b       Bandwidth performance analysis (with -f, compare values against second file)
+   -d       Side by side diff (must also specify -f)
+   -f file  Specify second file for two agent metrics summary file analysis
+   -h       help
+   -o       Overall time usage performance analysis (with -f, compare values against second file)
+   -u       Usage record performance analysis (with -f, compare values against second file)
 
 EOF
    exit 0
@@ -59,11 +61,13 @@ EOF
 # Get metrics category
 # args:
 # $1 - category
+# $2 - file
 function getMetricsCategory () {
    category=$1
+   file=$2
 
    # Truncate result to produce integer for comparison in displayPerfRanking
-   result=`grep "$category" $metricsFile | cut -d'(' -f2 | cut -d'.' -f1`
+   result=`grep "$category" $file | cut -d'(' -f2 | cut -d'.' -f1`
 
    # Mark incomplete metrics file with special result value of "failed" instead of a number
    if [ "x$result" == "x" ]; then
@@ -73,77 +77,230 @@ function getMetricsCategory () {
    echo $result
 }
 
+# Calculate percentage change for two metrics file comparison
+# args:
+# $1 - categoryTime1
+# $2 - categoryTime2
+function calcPercentChange () {
+   categoryTime1=$1
+   categoryTime2=$2
+
+   if [ $categoryTime1 -gt $categoryTime2 ]; then
+      time2=$categoryTime1
+      time1=$categoryTime2
+   else
+      time1=$categoryTime1
+      time2=$categoryTime2
+   fi
+
+   if [ $time2 -gt 0 ]; then
+      pc=$(( ((((time2 - time1) * 1000) / time2) + (time2 > time1 ? 5 : -5)) / 10 ))
+   else
+      # Handle corner case of both times are zero
+      pc=0
+   fi
+
+   echo $pc
+}
+
 # Display performance ranking for specified category
 # args:
 # $1 - category name
-# $2 - usage percent
-# $3 - less than label
-# $4 - less than percent
-# $5 - greater than label
-# $6 - greater than percent
+# $2 - category value
+# $3 - category units
+# $4 - less than label
+# $5 - less than value
+# $6 - greater than label
+# $7 - greater than value
 function displayPerfRanking () {
    categoryName=$1
-   usagePercent=$2
-   lessThanLabel=$3
-   lessThanPercent=$4
-   greaterThanLabel=$5
-   greaterThanPercent=$6
+   categoryValue=$2
+   categoryUnits=$3
+   lessThanLabel=$4
+   lessThanValue=$5
+   greaterThanLabel=$6
+   greaterThanValue=$7
 
    # Trap for incomplete metrics file
-   if [ $usagePercent == "failed" ]; then
+   if [ $categoryValue == "failed" ]; then
       noSpacesName=`echo $categoryName | sed -e "s/ //g"`
       echo "Error: Category [$noSpacesName] missing from $metricsFile; aborting analysis"
       exit 1
    fi
 
-   if [ $usagePercent -gt $greaterThanPercent ]; then
+   if [ $categoryValue -gt $greaterThanValue ]; then
       label="$greaterThanLabel"
-   elif [ $usagePercent -lt $lessThanPercent ]; then
+   elif [ $categoryValue -lt $lessThanValue ]; then
       label="$lessThanLabel"
    else
       label="Acceptable"
    fi
 
-   echo "$categoryName : $label (${usagePercent}%)"
+   echo "$categoryName : $label (${categoryValue}$categoryUnits)"
 }
 
 # Get percent usage for overall time categories and display analysis
 function analyzeOverallTimeUsage () {
    # Extract percentages from metrics file
-   commandPercent=`getMetricsCategory "Command:"`
-   emakeRequestPercent=`getMetricsCategory "Emake request:"`
-   returnPercent=`getMetricsCategory "Return:"`
-   idlePercent=`getMetricsCategory "Idle:"`
-   endPercent=`getMetricsCategory "End:"`
+   commandPercent=`getMetricsCategory "Command:" $metricsFile`
+   emakeRequestPercent=`getMetricsCategory "Emake request:" $metricsFile`
+   returnPercent=`getMetricsCategory "Return:" $metricsFile`
+   idlePercent=`getMetricsCategory "Idle:" $metricsFile`
+   endPercent=`getMetricsCategory "End:" $metricsFile`
 
+   # Calculate percentage change if two metrics files specified
+   if [ $F_OPT -eq 1 ]; then
+      commandPercent2=`getMetricsCategory "Command:" $metricsFile2`
+      emakeRequestPercent2=`getMetricsCategory "Emake request:" $metricsFile2`
+      returnPercent2=`getMetricsCategory "Return:" $metricsFile2`
+      idlePercent2=`getMetricsCategory "Idle:" $metricsFile2`
+      endPercent2=`getMetricsCategory "End:" $metricsFile2`
+
+      commandPercentPC=`calcPercentChange $commandPercent $commandPercent2`
+      emakeRequestPercentPC=`calcPercentChange $emakeRequestPercent $emakeRequestPercent2`
+      returnPercentPC=`calcPercentChange $returnPercent $returnPercent2`
+      idlePercentPC=`calcPercentChange $idlePercent $idlePercent2`
+      endPercentPC=`calcPercentChange $endPercent $endPercent2`
+   fi
 
    # Display performance rankings
-   echo "Overall Time Usage Performance"
-   echo "=============================="
-   displayPerfRanking "Command      " $commandPercent Warning 50 Good 60
-   displayPerfRanking "Emake Request" $emakeRequestPercent Good 10 Warning 15
-   displayPerfRanking "Return       " $returnPercent Good 10 Warning 20
-   displayPerfRanking "Idle         " $idlePercent Good 10 Warning 20
-   displayPerfRanking "End          " $endPercent Good 10 Warning 20
-   echo
+   if [ $F_OPT -eq 0 ]; then
+      echo "Overall Time Usage Performance"
+      echo "=============================="
+      displayPerfRanking "Command      " $commandPercent "%" Warning 50 Good 60
+      displayPerfRanking "Emake Request" $emakeRequestPercent "%" Good 10 Warning 15
+      displayPerfRanking "Return       " $returnPercent "%" Good 10 Warning 20
+      displayPerfRanking "Idle         " $idlePercent "%" Good 10 Warning 20
+      displayPerfRanking "End          " $endPercent "%" Good 10 Warning 20
+      echo
+   else
+      echo "Overall Time Usage Performance Percentage Change"
+      echo "================================================"
+      displayPerfRanking "Command      " $commandPercentPC "%" Good 10 Warning 25
+      displayPerfRanking "Emake Request" $emakeRequestPercentPC "%" Good 10 Warning 25
+      displayPerfRanking "Return       " $returnPercentPC "%" Good 10 Warning 25
+      displayPerfRanking "Idle         " $idlePercentPC "%" Good 10 Warning 25
+      displayPerfRanking "End          " $endPercentPC "%" Good 10 Warning 25
+      echo
+   fi
 }
 
 # Get usage record categories and display analysis
 function analyzeUsageRecords () {
    # Extract percentages from metrics file
-   failedLookup=`getMetricsCategory "Failed lookup  "`
-   read=`getMetricsCategory "Read  "`
-   lookup=`getMetricsCategory "Lookup  "`
-   create=`getMetricsCategory "Create  "`
+   failedLookup=`getMetricsCategory "Failed lookup  " $metricsFile`
+   read=`getMetricsCategory "Read  " $metricsFile`
+   lookup=`getMetricsCategory "Lookup  " $metricsFile`
+   create=`getMetricsCategory "Create  " $metricsFile`
+
+   # Calculate percentage change if two metrics files specified
+   if [ $F_OPT -eq 1 ]; then
+      failedLookup2=`getMetricsCategory "Failed lookup  " $metricsFile2`
+      read2=`getMetricsCategory "Read  " $metricsFile2`
+      lookup2=`getMetricsCategory "Lookup  " $metricsFile2`
+      create2=`getMetricsCategory "Create  " $metricsFile2`
+
+      failedLookupPC=`calcPercentChange $failedLookup $failedLookup2`
+      readPC=`calcPercentChange $read $read2`
+      lookupPC=`calcPercentChange $lookup $lookup2`
+      createPC=`calcPercentChange $create $create2`
+   fi
 
    # Display performance rankings
-   echo "Usage Records Performance"
-   echo "========================="
-   displayPerfRanking "Failed Lookup" $failedLookup Good 50 Warning 60
-   displayPerfRanking "Read         " $read Good 25 Warning 30
-   displayPerfRanking "Lookup       " $lookup Good 10 Warning 15
-   displayPerfRanking "Create       " $create Good 2 Warning 10
-   echo
+   if [ $F_OPT -eq 0 ]; then
+      echo "Usage Records Performance"
+      echo "========================="
+      displayPerfRanking "Failed Lookup" $failedLookup "%" Good 50 Warning 60
+      displayPerfRanking "Read         " $read "%" Good 25 Warning 30
+      displayPerfRanking "Lookup       " $lookup "%" Good 10 Warning 15
+      displayPerfRanking "Create       " $create "%" Good 2 Warning 10
+      echo
+   else
+      echo "Usage Records Performance Percentage Change"
+      echo "==========================================="
+      displayPerfRanking "Failed Lookup" $failedLookupPC "%" Good 10 Warning 25
+      displayPerfRanking "Read         " $readPC "%" Good 10 Warning 25
+      displayPerfRanking "Lookup       " $lookupPC "%" Good 10 Warning 25
+      displayPerfRanking "Create       " $createPC "%" Good 10 Warning 25
+      echo
+   fi
+}
+
+# Get bandwidth value (in MB/s)
+# args:
+# $1 - category
+# $2 - file
+# $3 - total (optional)
+function getBandwidthValue () {
+   category=$1
+   file=$2
+   total=$3
+
+   # Truncate result to produce integer for comparison in displayPerfRanking
+   if [ -z $total ]; then
+      result=`grep "$category" $file | cut -d',' -f3 | sed -e 's/^ *//' | cut -d" " -f1 | cut -d'.' -f1`
+   else
+      # Skip down to get total
+      result=`grep -A 3 "$category" $file | grep "Total:" | cut -d',' -f3 | sed -e 's/^ *//' | cut -d" " -f1 | cut -d'.' -f1`
+   fi
+
+   # Mark incomplete metrics file with special result value of "failed" instead of a number
+   if [ "x$result" == "x" ]; then
+      result="failed"
+   fi
+
+   echo $result
+}
+
+# Get bandwith performance
+function analyzeBandwidth() {
+   # Extract bandwidth values from metrics files
+   netFromEmake=`getBandwidthValue "Network from emake:" $metricsFile`
+   netToEmake=`getBandwidthValue "Network to emake:" $metricsFile`
+   netFromAgents=`getBandwidthValue "Network from agents:" $metricsFile`
+   netToAgents=`getBandwidthValue "Network to agents:" $metricsFile`
+   efsDiskReads=`getBandwidthValue "EFS disk reads" $metricsFile 1`
+   efsDiskWrites=`getBandwidthValue "EFS disk writes" $metricsFile 1`
+
+   # Calculate percentage change if two metrics files specified
+   if [ $F_OPT -eq 1 ]; then
+      netFromEmake2=`getBandwidthValue "Network from emake:" $metricsFile2`
+      netToEmake2=`getBandwidthValue "Network to emake:" $metricsFile2`
+      netFromAgents2=`getBandwidthValue "Network from agents:" $metricsFile2`
+      netToAgents2=`getBandwidthValue "Network to agents:" $metricsFile2`
+      efsDiskReads2=`getBandwidthValue "EFS disk reads" $metricsFile2 1`
+      efsDiskWrites2=`getBandwidthValue "EFS disk writes" $metricsFile2 1`
+
+      netFromEmakePC=`calcPercentChange $netFromEmake $netFromEmake2`
+      netToEmakePC=`calcPercentChange $netToEmake $netToEmake2`
+      netFromAgentsPC=`calcPercentChange $netFromAgents $netFromAgents2`
+      netToAgentsPC=`calcPercentChange $netToAgents $netToAgents2`
+      efsDiskReadsPC=`calcPercentChange $efsDiskReads $efsDiskReads2`
+      efsDiskWritesPC=`calcPercentChange $efsDiskWrites $efsDiskWrites2`
+   fi
+
+   # Display performance rankings
+   if [ $F_OPT -eq 0 ]; then
+      echo "Bandwidth Performance"
+      echo "====================="
+      displayPerfRanking "Network from emake " $netFromEmake "MB/s" Warning 5 Good 12
+      displayPerfRanking "Network to emake   " $netToEmake "MB/s" Warning 5 Good 12
+      displayPerfRanking "Network from agents" $netFromAgents "MB/s" Warning 5 Good 12
+      displayPerfRanking "Network to agents  " $netToAgents "MB/s" Warning 5 Good 12
+      displayPerfRanking "EFS disk reads     " $efsDiskReads "MB/s" Warning 30 Good 100
+      displayPerfRanking "EFS disk writes    " $efsDiskWrites "MB/s" Warning 10 Good 50
+      echo
+   else
+      echo "Bandwidth Performance Percentage Change"
+      echo "======================================="
+      displayPerfRanking "Network from emake " $netFromEmakePC "%" Good 10 Warning 25
+      displayPerfRanking "Network to emake   " $netToEmakePC "%" Good 10 Warning 25
+      displayPerfRanking "Network from agents" $netFromAgentsPC "%" Good 10 Warning 25
+      displayPerfRanking "Network to agents  " $netToAgentsPC "%" Good 10 Warning 25
+      displayPerfRanking "EFS disk reads     " $efsDiskReadsPC "%" Good 10 Warning 25
+      displayPerfRanking "EFS disk writes    " $efsDiskWritesPC "%" Good 10 Warning 25
+      echo
+   fi
 }
 
 # Show side by side diff
@@ -151,7 +308,7 @@ function sideBySideDiff () {
    echo "Side by Side Diff"
    echo "================="
    echo
-   diff -y -W 160 $metricsFile $dstMetricsFile
+   diff -y -W 160 $metricsFile $metricsFile2
 }
 
 
@@ -162,22 +319,30 @@ function sideBySideDiff () {
 ########################################
 
 # Option defaults
-NO_OPT=1 # indicates if no options set
+NO_OPT=1 # indicates no options set
+B_OPT=0
 D_OPT=0
+F_OPT=0
 O_OPT=0
 U_OPT=0
 
 # Parse command line options
-while getopts "ad:hou" opt; do
+while getopts "abdf:hou" opt; do
    case $opt in
 
-   a) O_OPT=1
+   a) B_OPT=1
+      O_OPT=1
       U_OPT=1
       NO_OPT=0
       ;;
-   d) dstMetricsFile="$OPTARG"
-      D_OPT=1
+   b) B_OPT=1
       NO_OPT=0
+      ;;
+   d) D_OPT=1
+      NO_OPT=0
+      ;;
+   f) metricsFile2="$OPTARG"
+      F_OPT=1
       ;;
    h) usage
       exit 0
@@ -199,7 +364,7 @@ if [ $NO_OPT -eq 1 ]; then
    O_OPT=1
 fi
 
-# Everything else is make option related
+# What's left is the metrics file
 shift $(($OPTIND - 1))
 
 # Get metrics summary file
@@ -208,12 +373,6 @@ metricsFile=$1
 # Make sure user has specified a metrics summary file
 if [ "x$metricsFile" == "x" ] ; then
     echo "Error: Must specify an agent metrics summary file using agentMetrics.sh" 
-    exit 1
-fi
-
-# If -d option specified, make sure user has specified a destination metrics summary file for diffing
-if [ $D_OPT -eq 1 -a "x$dstMetricsFile" == "x" ] ; then
-    echo "Error: Must specify a second agent metrics summary file when specifying -d option" 
     exit 1
 fi
 
@@ -227,7 +386,17 @@ if [ $U_OPT -eq 1 ]; then
    analyzeUsageRecords
 fi
 
+# Bandwith performance
+if [ $B_OPT -eq 1 ]; then
+   analyzeBandwidth
+fi
+
 # Display side by side diff of specified metrics files
 if [ $D_OPT -eq 1 ]; then
-   sideBySideDiff 
+   if [ $F_OPT -eq 1 ]; then
+      sideBySideDiff 
+   else
+      echo "Error: Must specify -f option when using -d" 
+      exit 1
+   fi
 fi
